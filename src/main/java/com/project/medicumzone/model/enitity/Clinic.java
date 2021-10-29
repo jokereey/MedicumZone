3f6c325309d0119251721ea26148042352be486e
jokereey <w.mlynarczyk20@gmail.com>
2021-10-30 01:24:00 +0200

    clinic basic fields

--- src/main/java/com/project/medicumzone/model/enitity/Clinic.java
+++ src/main/java/com/project/medicumzone/model/enitity/Clinic.java
@@ -1,4 +1,13 @@
 package com.project.medicumzone.model.enitity;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity(name = "Clinic")
+@Table(name = "clinic")
 public class Clinic {
+    @Id
+    private Long clinicId;
+    private City city;
 }
