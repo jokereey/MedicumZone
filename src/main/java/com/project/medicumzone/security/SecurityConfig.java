1b2695e16c9263dd601adf0e6c0a56f05c81c959
Wiktor <w.mlynarczyk20@gmail.com>
2021-11-14 01:39:20 +0100

    swagger configuration

--- src/main/java/com/project/medicumzone/security/SecurityConfig.java
+++ src/main/java/com/project/medicumzone/security/SecurityConfig.java
@@ -20,6 +20,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -29,6 +32,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private JWTTokenHelper jwtTokenHelper;
+
     @Autowired
     private AuthenticationEntryPoint authenticationEntryPoint;
 
@@ -40,10 +44,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
+
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().exceptionHandling()
                 .authenticationEntryPoint(authenticationEntryPoint).and()
-                .authorizeRequests((request) -> request.antMatchers( "/api/cities","/api/v1/auth/login","/api/users/add").permitAll()
-                        .antMatchers(HttpMethod.OPTIONS, "/**").permitAll().anyRequest().authenticated())
+                .authorizeRequests((request) -> request.antMatchers
+                                ("/api/cities",
+                                        "/api/v1/auth/login",
+                                        "/api/users/add",
+                                        "/swagger-ui/**",
+                                        "/v3/api-docs",
+                                        "/webjars/**",
+                                        "/swagger-resources/**",
+                                        "/api/clinic/doctor"
+                                )
+                        .permitAll()
+                        .antMatchers(HttpMethod.OPTIONS, "/api/**").permitAll().anyRequest().authenticated())
                 .addFilterBefore(new JWTAuthenticationFilter(myUserDetailsService, jwtTokenHelper),
                         UsernamePasswordAuthenticationFilter.class);
 
@@ -56,8 +72,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         auth.userDetailsService(myUserDetailsService).passwordEncoder(passwordEncoder());
 
     }
+
     @Bean
-    public PasswordEncoder passwordEncoder(){
-        return  new BCryptPasswordEncoder();
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
