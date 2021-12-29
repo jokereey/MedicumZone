a14ef85fe7a0bf538937dfe1e493441b1b0b2bc4
jokereey <w.mlynarczyk20@gmail.com>
2021-12-29 21:13:10 +0100

    temporary header component design

--- src/ui/medicumzone-ui/src/app/components/header/header.component.ts
+++ src/ui/medicumzone-ui/src/app/components/header/header.component.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  facebookLink: string ='https://facebook.com/medicum-zone';
+  instagramLink: string ='https://instagram.com/medicum-zone';
+  twitterLink: string ='https://twitter.com/medicum-zone';
+  headerText : string = "Zadbaj o swoje zdrowie już dziś!"
+  phoneNumber: string = '+12 630 80 00';
   constructor() { }
 
   ngOnInit(): void {
+  }
+  onLoginClick(){
+  console.log('not implemented yet');
+  }
+  onRegistryClick(){
+    console.log('not implemented yet');
   }
 
 }
