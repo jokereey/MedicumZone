165b5280dcf005b708cfbd34cebd5bab8c56d2be
Wiktor <w.mlynarczyk20@gmail.com>
2022-01-08 23:30:25 +0100

    footer component with email input for the newsletter

--- src/ui/medicumzone-ui/src/app/components/footer/footer.component.ts
+++ src/ui/medicumzone-ui/src/app/components/footer/footer.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {ToastService} from "../shared/toast/toast-service";
 
 @Component({
   selector: 'app-footer',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  // @ts-ignore
+  @ViewChild('email', {static: true}) emailValue: ElementRef;
+
+  buttonState = false;
+  buttonValue = '';
+
+  constructor(private toastService: ToastService) {
+  }
 
   ngOnInit(): void {
+  }
+
+  onNewsletter() {
+    console.log(this.emailValue.nativeElement.value);
+    this.showDanger("error");
+  }
+
+  showSuccess() {
+    this.toastService.show('Email został pomyślnie dodany do newslettera!', {
+      classname: 'bg-primary text-light',
+      delay: 10000
+    });
+  }
+
+  showDanger(dangerTpl: any) {
+    this.toastService.show("Brak implementacji", {classname: 'bg-danger text-light', delay: 5000});
   }
 
 }
