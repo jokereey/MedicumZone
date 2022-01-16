3865b3b3669e11421ebfafdedb071282dfff0809
michu9971 <qrk1@interia.pl>
2022-01-16 21:56:57 +0100

    Enable web security

--- src/main/java/com/project/medicumzone/security/SecurityConfig.java
+++ src/main/java/com/project/medicumzone/security/SecurityConfig.java
@@ -24,7 +24,7 @@ import java.util.Arrays;
 import java.util.List;
 
 @Configuration
-//@EnableWebSecurity
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
