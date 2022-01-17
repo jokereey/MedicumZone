38f1b4b01cd221cea79da45c632b1698fd68859f
Wiktor <w.mlynarczyk20@gmail.com>
2022-01-17 23:11:09 +0100

    jwt token is now based on email. few simple controllers

--- src/main/java/com/project/medicumzone/ui/controller/DoctorController.java
+++ src/main/java/com/project/medicumzone/ui/controller/DoctorController.java
@@ -1,11 +1,15 @@
 package com.project.medicumzone.ui.controller;
 
+import com.project.medicumzone.io.enitity.Doctor;
 import com.project.medicumzone.service.DoctorService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @AllArgsConstructor
 @RestController
@@ -15,4 +19,9 @@ public class DoctorController {
 
     @Autowired
     private DoctorService doctorService;
+
+    @GetMapping
+    public List<Doctor> getAll(){
+     return doctorService.getAll() ;
+    }
 }
