45bb4e802aaab5fcfe8a5bdbe746a558465187b1
Wiktor <w.mlynarczyk20@gmail.com>
2022-01-29 20:01:02 +0100

    singup request

--- src/ui/medicumzone-ui/src/app/model/user/user.ts
+++ src/ui/medicumzone-ui/src/app/model/user/user.ts
@@ -2,3 +2,13 @@ export interface LoginRequest{
   username: string;
   password: string;
 }
+export interface SignUpRequest{
+  name: string;
+  surname: string;
+  PESEL: string,
+  dob: Date,
+  phoneNumber: string,
+  email: string,
+  password: string,
+  newsletter: boolean
+}
