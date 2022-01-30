b987fbe88f6847ae63cd673548422f9d656ebfb8
Wiktor <w.mlynarczyk20@gmail.com>
2022-01-30 19:06:12 +0100

    user class

--- src/ui/medicumzone-ui/src/app/model/user/user.ts
+++ src/ui/medicumzone-ui/src/app/model/user/user.ts
@@ -1,3 +1,20 @@
+export class  User{
+   constructor(private _token: string,
+               private _expiresIn:Date,
+               public id: string,
+               public name: string) {
+   }
+  get token() {
+    if (!this._expiresIn || new Date() > this._expiresIn) {
+      return null;
+    }
+    return this._token;
+  }
+  get expiresIn(){
+     return this._expiresIn;
+  }
+}
+
 export interface LoginRequest{
   username: string;
   password: string;
@@ -12,3 +29,4 @@ export interface SignUpRequest{
   password: string,
   newsletter: boolean
 }
+
