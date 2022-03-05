817c6901d88c7666b73d9f774d6ab4cd36c06cd8
Wiktor <w.mlynarczyk20@gmail.com>
2022-03-05 10:25:56 +0100

    twilio sms sender update

--- src/main/java/com/project/medicumzone/MedicumZoneApplication.java
+++ src/main/java/com/project/medicumzone/MedicumZoneApplication.java
@@ -1,23 +1,41 @@
 package com.project.medicumzone;
 
+import com.project.medicumzone.config.TwilioConfig;
+import com.project.medicumzone.twilio.SmsRequest;
+import com.project.medicumzone.twilio.TwilioService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.vault.config.VaultAutoConfiguration;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 import javax.annotation.PostConstruct;
 
 @SpringBootApplication
+@Slf4j
+public class MedicumZoneApplication implements CommandLineRunner {
 
-public class MedicumZoneApplication {
 
+    private final TwilioConfig twilioConfig;
+    private final TwilioService service;
+
+    public MedicumZoneApplication(TwilioConfig twilioConfig, TwilioService service) {
+        this.twilioConfig = twilioConfig;
+        this.service = service;
+    }
 
     public static void main(String[] args) {
-        SpringApplication.run(MedicumZoneApplication.class, args);
+      SpringApplication.run(MedicumZoneApplication.class, args);
+
     }
 
-    @PostConstruct
-    protected void init() {
-    }
+    @Override
+    public void run(String... args) throws Exception {
 
+    }
 }
--- src/main/java/com/project/medicumzone/twilio/TwilioSmsSender.java
+++ src/main/java/com/project/medicumzone/twilio/TwilioSmsSender.java
@@ -5,14 +5,21 @@ import com.project.medicumzone.utils.PhoneNumberValidator;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.rest.api.v2010.account.MessageCreator;
 import com.twilio.type.PhoneNumber;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 
 @Service("twilio")
+@Slf4j
 public class TwilioSmsSender implements SmsSender{
 
     private final TwilioConfig twilioConfiguration;
     private final PhoneNumberValidator phoneNumberValidator;
+
+    @Value("${twilio.sender.enabled}")
+    private boolean isEnabled;
+    private boolean isSent = false;
 
     public TwilioSmsSender(TwilioConfig twilioConfiguration, PhoneNumberValidator phoneNumberValidator) {
         this.twilioConfiguration = twilioConfiguration;
@@ -23,18 +30,21 @@ public class TwilioSmsSender implements SmsSender{
     public boolean sendSms(SmsRequest smsRequest) {
         if(phoneNumberValidator.test(smsRequest.getPhoneNumber())) {
             PhoneNumber to = new PhoneNumber(smsRequest.getPhoneNumber());
-            PhoneNumber from = new PhoneNumber(twilioConfiguration.getTwilioAvailableNumber());
+            PhoneNumber from = new PhoneNumber(twilioConfiguration.getMyTwilioNumber());
             String message = smsRequest.getMessage();
             MessageCreator creator = Message.creator(
                     to,
                     from,
                     message
             );
-            creator.create();
-            System.out.println("true");
-            return true;
+            if(isEnabled){
+                creator.create();
+            }
+            log.info("Message has been sent successfully. Twilio is disabled");
+            isSent = true;
+            return isSent;
         }
-        System.out.println("FALSE");
-        return false;
+        log.error("Phone number is invalid.");
+        return isSent;
     }
 }
