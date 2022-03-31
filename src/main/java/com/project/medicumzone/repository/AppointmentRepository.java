f4c27b97257d7767ce4df34a850f5f8b71d3ff9c
Wiktor <w.mlynarczyk20@gmail.com>
2022-03-31 20:20:30 +0200

    new appointment error case

--- src/main/java/com/project/medicumzone/repository/AppointmentRepository.java
+++ src/main/java/com/project/medicumzone/repository/AppointmentRepository.java
@@ -1,10 +1,19 @@
 package com.project.medicumzone.repository;
 
+import com.project.medicumzone.io.enitity.AppUser;
 import com.project.medicumzone.io.enitity.Appointment;
+import com.project.medicumzone.io.enitity.Clinic;
+import com.project.medicumzone.io.enitity.Doctor;
 import com.project.medicumzone.io.id.AppointmentID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.print.Doc;
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Repository
 public interface AppointmentRepository extends JpaRepository<Appointment, AppointmentID> {
+
+    boolean existsByAppointmentDateAndDoctorAndClinic(LocalDateTime localDateTime, Doctor doctor, Clinic clinic);
 }
